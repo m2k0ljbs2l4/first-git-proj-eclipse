f9642023f0a0f66293b62d913b8a8b007ae576a6
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-06 02:20:16 +0500

    Change class FirstGit add sout

--- gittest/java/gittest/FirstGit.java
+++ gittest/java/gittest/FirstGit.java
@@ -5,6 +5,7 @@ public class FirstGit {
 	public static void main(String[] args) {
 		System.out.println("GIT DEVELOP");
         System.out.println("LEADNING!");
+        System.out.println("ABOBA");
 	}
 
 }
